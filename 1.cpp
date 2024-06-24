2157bb69dcaf153bcfc2a26bbbc6556a137605d8
RajendraPIssar <158245418+RajendraPIssar@users.noreply.github.com>
2024-06-25 04:52:35 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,89 @@
+/* 
+
+1. How can you output “Physics”and “Wallah” in two different lines in C++? 
+
+2. Print 10 using 2 positive numbers less than 6 in C++ as output.
+
+3. How much space does following data types take ?
+    1. int 2. bool 3. float
+
+4. What is the output of this program?
+    void main()
+    {
+        int a=4;
+        int b=5;
+        a++;b--;
+        cout<<++a<<” “<<b–-;
+    }
+ 
+5. WAP to find the circumference of a circle with radius 10 in C++.
+
+6. How many of these can be a variable name?
+    01PWskills 
+    _FLOAT
+    int
+    FLOAT
+    You will succeed
+
+*/
+
+
+
+/* 
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Physics\nWallah";
+    return 0;
+} 
+*/
+
+/* 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter x : ";
+    cin>>x;
+
+    int y;
+    cout<<"Enter y : ";
+    cin>>y;
+
+    int sum = x+y;
+    cout<<sum;
+    
+    return 0;
+}
+*/
+
+/* 
+int : 4
+bool : 1
+float : 4  
+*/
+
+/*
+a = 6
+b = 4
+*/
+
+/* 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int r=10;
+    int circumference = 2*3.14*r;
+    cout<<"Circumference = "<<circumference;
+    
+    return 0;
+} 
+*/
+
+/* 
+    _FLOAT
+    FLOAT
+ */
